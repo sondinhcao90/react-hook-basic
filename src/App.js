5f651118193339984021f7b20cfec1f4f475cf91
sonvd <vdson.hut@gmail.com>
2021-05-08 16:44:50 +0700

    add colorBox componet example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to react hooks!</h1>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue'];
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    const [color, setColor] = useState('deeppink')
+
+    function handleBoxClick() {
+        // get random color
+        const newColor = getRandomColor();
+        setColor(newColor);
+    }
+
+    return (
+        <div className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+            COLOR BOX
+        </div>
+    );
+}
+
+export default ColorBox;

b95e8084b25a8b38f36dd135e1db192004686f61
sonvd <vdson.hut@gmail.com>
2021-05-09 12:19:28 +0700

    -example useState
    remove function list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'So 1' },
+    { id: 2, title: 'So 2' },
+    { id: 3, title: 'So 3' }]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <h1>Welcome to react hooks!</h1>
-      <ColorBox />
+      <h1>React hooks todoList</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
--- src/components/ColorBox/index.jsx
+++ src/components/ColorBox/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './ColorBox.scss'
+import './ColorBox.scss';
 
 ColorBox.propTypes = {
 

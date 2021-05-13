57368c8881810edd59ec5e5067ac46e231db0292
sonvd <vdson.hut@gmail.com>
2021-05-13 13:03:28 +0700

    - add useEffect example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
@@ -8,6 +9,29 @@ function App() {
     { id: 1, title: 'So 1' },
     { id: 2, title: 'So 2' },
     { id: 3, title: 'So 3' }]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJson = await response.json();
+        console.log({ responseJson });
+        const { data } = responseJson;
+        setPostList(data);
+      } catch (error) {
+        console.log('Faild to fetch data ', error.message);
+      }
+    }
+    console.log('Post list effect');
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log('Todo list effect');
+  });
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x => x.id === todo.id);
@@ -30,9 +54,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>React hooks todoList</h1>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      <h1>React hooks PostList</h1>
+      <PostList posts={postList} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
     </div>
   );
 }
